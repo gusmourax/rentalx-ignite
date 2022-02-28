0ba5706ff930cc00680062a13342496a5cb13691
Gustavo Moura <gustavomoura5555@outlook.com>
2022-02-28 19:03:57 -0300

    Rota index

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -14,6 +14,7 @@ router.use('/specifications', specificationsRoutes);
 router.use('/users', usersRoutes);
 router.use('/cars', carsRoutes);
 router.use('/rentals', rentalRoutes);
+router.get('/', (req, res) => res.send('Hello World!'));
 router.use(authenticateRoutes);
 
 export { router };
